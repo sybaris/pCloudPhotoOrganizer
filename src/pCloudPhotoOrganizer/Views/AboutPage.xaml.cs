15859108b2e54fbeef6ad293700c4a912c9c009e
Sybaris <Sybaris@Club-Internet.Fr>
2025-12-24 12:06:56 +0100

    About : Android version added

--- src/pCloudPhotoOrganizer/Views/AboutPage.xaml.cs
+++ src/pCloudPhotoOrganizer/Views/AboutPage.xaml.cs
@@ -1,4 +1,5 @@
 using Microsoft.Maui.ApplicationModel;
+using Microsoft.Maui.Devices;
 
 namespace pCloudPhotoOrganizer.Views;
 
@@ -12,6 +13,12 @@ public partial class AboutPage : ContentPage
 
         AppNameLabel.Text = AppInfo.Current.Name;
         VersionLabel.Text = $"Version {AppInfo.Current.VersionString}";
+
+        if (DeviceInfo.Current.Platform == DevicePlatform.Android)
+        {
+            AndroidVersionLabel.Text = $"Version Android {DeviceInfo.Current.VersionString}";
+            AndroidVersionLabel.IsVisible = true;
+        }
     }
 
     private async void OnLinkTapped(object sender, TappedEventArgs e)
